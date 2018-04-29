761d52515f84dd5aeb803490f3bbfc1d555ec59e
Photon Cat <lkstc112233@gmail.com>
2018-04-29 19:58:42 -0400

    Initial update

---
+++ Core-beta/Core-beta/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Core-beta
+//
+//  Created by Photon Cat on 2018/4/29.
+//  Copyright © 2018 Must Studio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
