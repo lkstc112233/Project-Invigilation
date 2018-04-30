48e1a63b48cc38c8056b942d3464d9a6d7f900b9
Photon Cat <lkstc112233@gmail.com>
2018-04-29 20:20:06 -0400

    Player

---
+++ Core-beta/Core-beta/Player.cpp
@@ -0,0 +1,12 @@
+//
+//  Player.cpp
+//  Core-beta
+//
+//  Created by Photon Cat on 2018/4/29.
+//  Copyright © 2018 Must Studio. All rights reserved.
+//
+
+#include "Player.hpp"
+namespace INVIGILATION_CORE {
+    
+}
---
+++ Core-beta/Core-beta/Player.hpp
@@ -0,0 +1,18 @@
+//
+//  Player.hpp
+//  Core-beta
+//
+//  Created by Photon Cat on 2018/4/29.
+//  Copyright © 2018 Must Studio. All rights reserved.
+//
+
+#ifndef Player_hpp
+#define Player_hpp
+
+namespace INVIGILATION_CORE {
+    class Player{
+        
+    };
+}
+
+#endif /* Player_hpp */
