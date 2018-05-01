696c6cc3b1ca356917df7ac3502df76eed57e6d5
Quantum cat <lkstc112233@users.noreply.github.com>
2018-05-01 02:00:28 -0400

    Added vs support

--- Core-beta/Core-beta/Card.cpp
+++ Core-beta/Core-beta/Card.cpp
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Must Studio. All rights reserved.
 //
 
+#include "stdafx.h"
 #include "Card.hpp"
 namespace INVIGILATION_CORE {
     
--- Core-beta/Core-beta/Core.cpp
+++ Core-beta/Core-beta/Core.cpp
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Must Studio. All rights reserved.
 //
 
+#include "stdafx.h"
 #include "Core.hpp"
 namespace INVIGILATION_CORE {
     Core::Core() {
