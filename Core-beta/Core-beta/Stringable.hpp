d3cb42c75cb21be94593a7c8797bc83eef7a4e8b
Photon Cat <lkstc112233@gmail.com>
2018-05-06 17:39:22 -0400

    Added a stringable interface

---
+++ Core-beta/Core-beta/Stringable.hpp
@@ -0,0 +1,28 @@
+//
+//  Stringable.hpp
+//  Core-beta
+//
+//  Created by Photon Cat on 2018/5/6.
+//  Copyright © 2018 Must Studio. All rights reserved.
+//
+
+#ifndef Stringable_h
+#define Stringable_h
+
+#include <string>
+
+namespace INVIGILATION_CORE
+{
+    /**
+     * This is an interface indicating that the class can be represented into a string.
+     * The converted string is human-readable, so it can be used to make automatic tests.
+     * upon converted the string is stored in a std::string structure.
+     */
+    class Stringable
+    {
+    public:
+        virtual std::string toString() = 0;
+    };
+}
+
+#endif /* Stringable_h */
