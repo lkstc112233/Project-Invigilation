6fc8655867ad7e662254e0ea8eb2c0a75b678f5a
Photon Cat <lkstc112233@gmail.com>
2018-05-06 21:06:28 -0400

    implemented a function to replace all occurrence in a string.

---
+++ Core-beta/Core-beta/utility.cpp
@@ -0,0 +1,31 @@
+//
+//  utility.cpp
+//  Core-beta
+//
+//  Created by Photon Cat on 2018/5/6.
+//  Copyright © 2018 Must Studio. All rights reserved.
+//
+
+#include "utility.hpp"
+
+
+namespace INVIGILATION_CORE {
+    namespace UTILITY{
+        /**
+         * Replaces all occurrence of pattern in input with replacement.
+         */
+        std::string& replaceAll(std::string &input, const std::string &pattern, const std::string &replacement)
+        {
+            if (pattern.size() == 0)
+                return input;
+            auto pos = input.find(pattern);
+            while (pos != std::string::npos)
+            {
+                input.replace(pos, pattern.size(), replacement);
+                pos += replacement.size();
+                pos = input.find(pattern, pos);
+            }
+            return input;
+        }
+    }
+}
---
+++ Core-beta/Core-beta/utility.hpp
@@ -0,0 +1,20 @@
+//
+//  utility.hpp
+//  Core-beta
+//
+//  Created by Photon Cat on 2018/5/6.
+//  Copyright © 2018 Must Studio. All rights reserved.
+//
+
+#ifndef utility_hpp
+#define utility_hpp
+
+#include <string>
+
+namespace INVIGILATION_CORE {
+    namespace UTILITY{
+        std::string replaceAll(const std::string &input, const std::string &pattern, const std::string &replacement);
+    }
+}
+
+#endif /* utility_hpp */
