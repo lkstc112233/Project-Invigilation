405b9378d9a3fa333c9fd4cbc022fb482118bd0c
Photon Cat <lkstc112233@gmail.com>
2018-05-10 17:59:08 -0400

    Added seed to prevent randomness

--- Core-beta/Core-beta-test/main.cpp
+++ Core-beta/Core-beta-test/main.cpp
@@ -9,12 +9,14 @@
 #include "stdafx.h"
 #include <iostream>
 #include <fstream>
+#include <random>
 #include "../Core-beta/Card.hpp"
 #include "../Core-beta/Player.hpp"
 #include "../Core-beta/CardFactory.hpp"
 #include "../Core-beta/Core.hpp"
 #include "../Core-beta/ConsoleInterface.hpp"
 #include "../Core-beta/Action.hpp"
+#include "../Core-beta/utility.hpp"
 
 void deckBuildingTest()
 {
@@ -32,7 +34,17 @@ void interactionTest()
         std::cout << "ActionReceived" << std::endl;
 }
 
+namespace INVIGILATION_CORE
+{
+    namespace UTILITY
+    {
+        extern std::mt19937 RandomDevice19937ForShuffling;
+    }
+}
+
 int main(int argc, const char * argv[]) {
+    INVIGILATION_CORE::UTILITY::RandomDevice19937ForShuffling.seed(42);
+    deckBuildingTest();
     return 0;
 }
 
